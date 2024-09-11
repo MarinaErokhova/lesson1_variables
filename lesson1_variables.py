de5fa51080c51419c434b2dce4b2affa03b00c37
MarinaErokhova <erokhovamn@gmail.com>
2024-09-11 23:30:24 +0300

    Add files via upload

---
+++ lesson1_variables.py
@@ -0,0 +1,5 @@
+homework = 12
+wasted_time = 1.5
+name_of_course = 'Python'
+lead_time1 = wasted_time / homework
+print('Курс: ', name_of_course, ', всего задач: ', homework, ', затрачено часов: ', wasted_time, ', среднее время выполнения: ', lead_time1)
